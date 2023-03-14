c4bf7e5bc5ff314b923ef36dc7f6df35ebdb682c
eylmaz <01173362@pw.edu.pl>
2023-03-14 14:38:25 +0300

    NotificationPassenger implementation

---
+++ shuttleasy/shuttleasy.LOGIC/Logics/NotificationPassengers/NotificationPassengerLogic.cs
@@ -0,0 +1,19 @@
+﻿using shuttleasy.DAL.EFRepositories.GeoPoints;
+using shuttleasy.DAL.EFRepositories.NotificationPassengers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shuttleasy.LOGIC.Logics.NotificationPassengers
+{
+    public class NotificationPassengerLogic : INotificationPassengerLogic
+    {
+        private INotificationPassengerRepository _notificationPassengerRepository;
+        public NotificationPassengerLogic(INotificationPassengerRepository notificationPassengerRepository)
+        {
+            _notificationPassengerRepository = notificationPassengerRepository;
+        }
+    }
+}
---
+++ shuttleasy/shuttleasy/Controllers/NotificationPassengerController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using shuttleasy.LOGIC.Logics.CompanyWorkers;
+using shuttleasy.LOGIC.Logics.GeoPoints;
+using shuttleasy.LOGIC.Logics;
+using shuttleasy.Services;
+using shuttleasy.LOGIC.Logics.NotificationPassengers;
+
+namespace shuttleasy.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class NotificationPassengerController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IPassengerLogic _passengerLogic;
+        private readonly ICompanyWorkerLogic _driverLogic;
+        private readonly INotificationPassengerLogic _notificationPassengerLogic;
+        private readonly IMapper _mapper;
+        public NotificationPassengerController(IUserService userService, IPassengerLogic passengerLogic, ICompanyWorkerLogic driverLogic, INotificationPassengerLogic notificationPassengerLogic,
+           IMapper mapper)
+        {
+            _userService = userService;
+            _passengerLogic = passengerLogic;
+            _driverLogic = driverLogic;
+            _notificationPassengerLogic = notificationPassengerLogic;
+            _mapper = mapper;
+        }
+    }
+}
